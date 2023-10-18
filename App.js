fd181d68e427cfdbee571ddd90bc89e8fa298be9
tans27 <trunganhcoder@gmail.com>
2023-10-18 16:38:56 +0700

    update

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { registerRootComponent } from 'expo';
 import HandleScreen from "./HandleScreen";
 import { SettingProvider } from "./screens/SettingsScreen/Context";
 import { DataProvider } from "./data/dataProvider";
@@ -40,3 +41,6 @@ export default function App() {
     </>
   );
 }
+
+
+registerRootComponent(App);
